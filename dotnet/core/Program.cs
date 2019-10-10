241faa00486a603a5373b3981cb10bb6d6176834
Dmitry Ilin <dimailinemail@gmail.com>
2019-10-10 01:13:28 -0400

    initial commit

---
+++ dotnet/core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ElectronCgi.DotNet;
+
+namespace core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new ConnectionBuilder()
+                .WithLogging()
+                .Build();
+            
+            connection.On<string, string>("greeting", name => "Hello " + name);
+            
+            connection.Listen();
+        }
+    }
+}
