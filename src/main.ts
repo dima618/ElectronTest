47eb0524762b7af6b3ce67a935e2f5f063dc9670
Dmitry Ilin <dimailinemail@gmail.com>
2019-10-10 01:22:18 -0400

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,15 @@ const { ConnectionBuilder } = require("electron-cgi");
 
 import { app, BrowserWindow } from "electron";
 
-let window: BrowserWindow | null;
+const window = require('./Window');
 
-const createWindow = () => {
-  window = new BrowserWindow({ width: 800, height: 600 });
+function main() {
+  let mainWindow = new window({
+    file: 'index.html'
+  })
+}
 
-  window.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file:",
-      slashes: true
-    })
-  );
-
-  window.on("closed", () => {
-    window = null;
-  });
-};
-
-app.on("ready", createWindow);
+app.on("ready", main);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
@@ -33,7 +23,7 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
   if (window === null) {
-    createWindow;
+    main;
   }
 });
 
